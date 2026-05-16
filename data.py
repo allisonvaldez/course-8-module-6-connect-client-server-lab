60c0403993a697d32267c5b0524a0679aeb1db58
Allison Valdez <allison.snipe@gmail.com>
2026-05-16 16:43:48 -0700

    setting up file

---
+++ data.py
@@ -0,0 +1,10 @@
+# Create a file to hold the data
+
+def get_sample_data():
+    events = [
+        {"id": 1, "title": "Event 1"},
+        {"id": 2, "title": "Event 2"},
+        {"id": 3, "title": "Event 3"}
+    ]
+
+    return events
--- server.py
+++ server.py
@@ -1,25 +1,30 @@
+# Import utilities and helper files
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from data import get_sample_data
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../client', static_url_path='')
 CORS(app)
 
-# Create a list called 'events' with a couple of sample event dictionaries
-# Each dictionary should have an 'id' and a 'title'
+"""
+Create a route for "/". This route should return a JSON welcome message. Make sure to use decorator and create a proper function.
+"""
+@app.route("/", methods=["GET"])
+def welcome():
+    return send_from_directory(app.static_folder, "index.html")
 
-# TASK: Create a route for "/"
-# This route should return a JSON welcome message
+"""
+Create a GET route for "/events". This route should return the full list of events as JSON.
+"""
 
-# TASK: Create a GET route for "/events"
-# This route should return the full list of events as JSON
-
-# TASK: Create a POST route for "/events"
-# This route should:
-# 1. Get the JSON data from the request
-# 2. Validate that "title" is provided
-# 3. Create a new event with a unique ID and the provided title
-# 4. Add the new event to the events list
-# 5. Return the new event with status code 201
+"""
+Create a POST route for "/events": This route should:
+    1. Get the JSON data from the request
+    2. Validate that "title" is provided
+    3. Create a new event with a unique ID and the provided title
+    4. Add the new event to the events list
+    5. Return the new event with status code 201
+"""
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=5555, debug=True)
